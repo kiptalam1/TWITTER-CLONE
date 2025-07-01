b41298e021099d745067448bf5274d0b750d266f
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-01 18:15:49 +0300

    feat: functionality to delete own user's  post.

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -31,3 +31,34 @@ export async function createPost(req, res) {
 		res.status(500).json({ error: "Internal server error" });
 	}
 }
+
+export async function deletePost(req, res) {
+	try {
+		const id = req.params.id;
+		const userId = req.user._id;
+
+		//fetch post from mongo;
+		const post = await Post.findById(id);
+		if (!post) return res.status(404).json({ error: "Post not found" });
+
+		// prevent user from deleting another user's post;
+		if (userId.toString() !== post.user.toString())
+			return res
+				.status(400)
+				.json({ error: "You cannot delete another user's posts" });
+
+		// if post has an image;
+		if (post.image) {
+			// delete from cloudinary;
+			const imageId = post.image.split("/").pop().split(".")[0];
+			await cloudinary.uploader.destroy(imageId);
+		}
+
+		// finally delete post from mongo;
+		await Post.findByIdAndDelete(id);
+		return res.status(200).json({ message: "Post was deleted successfully" });
+	} catch (error) {
+		console.error("Error deleting post", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/protectRoute.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+
 
 export default router;
