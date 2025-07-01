10d977882dd676aec518d4045084424a3d574d58
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-01 15:21:35 +0300

    db: create a posts model/schema.

---
+++ backend/models/post.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		text: {
+			type: String,
+		},
+		image: {
+			type: String,
+		},
+		likes: [
+			{
+				user: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User",
+					required: true,
+				},
+			},
+		],
+		comments: [
+			{
+				user: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User",
+					required: true,
+				},
+				text: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
