2b8ae71d01ce47dd0166eef78dd9200946769720
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-04 13:42:02 +0300

    feat: complete UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignupPage from "./pages/auth/signup/SignupPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 function App() {
 	return (
-		<>
-			<p className="text-5xl text-red-500 bg-amber-200">Hello from app jsx</p>
-		</>
+		<div className="flex max-w-6xl mx-auto">
+			<Sidebar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/signup" element={<SignupPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/notifications" element={<NotificationPage />} />
+				<Route path="/profile/:username" element={<ProfilePage />} />
+			</Routes>
+			<RightPanel />
+		</div>
 	);
 }
 
