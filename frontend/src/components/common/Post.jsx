29d5ce3d6ad6eb79c4ac4c57340b7ba491c35229
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-06 13:26:20 +0300

    feat: implement delete own post functionality.

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -1,3 +1,5 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 import { FaRegComment } from "react-icons/fa";
 import { BiRepost } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
@@ -5,19 +7,42 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Post = ({ post }) => {
+	const queryClient = useQueryClient();
+	const authUser = queryClient.getQueryData(["authUser"]);
 	const [comment, setComment] = useState("");
 	const postOwner = post.user;
 	const isLiked = false;
 
-	const isMyPost = true;
+	const isMyPost = authUser._id === post.user._id;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const { mutate: deletePost, isPending } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/${post._id}`, { method: "DELETE" });
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess: (data) => {
+			toast.success(data.message || "Deleted successfully");
+			// invalidate query to refetch the data;
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+	});
+
+	const handleDeletePost = () => {
+		deletePost();
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
@@ -49,10 +74,13 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className="flex justify-end flex-1">
-								<FaTrash
-									className="cursor-pointer hover:text-red-500"
-									onClick={handleDeletePost}
-								/>
+								{!isPending && (
+									<FaTrash
+										className="cursor-pointer hover:text-red-500"
+										onClick={handleDeletePost}
+									/>
+								)}
+								{isPending && <LoadingSpinner />}
 							</span>
 						)}
 					</div>
