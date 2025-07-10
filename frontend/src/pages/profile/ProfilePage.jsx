9d9be360ffe8caf7b6bb4ffa689969e696498683
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-10 14:55:09 +0300

    feat: fetch user profiles of users when clicked.

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 import Posts from "../../components/common/Posts";
 import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
@@ -11,6 +11,8 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
+import { formatMemberSinceDate } from "../../utils/date/index.js";
+import { useQuery } from "@tanstack/react-query";
 
 const ProfilePage = () => {
 	const [coverImg, setCoverImg] = useState(null);
@@ -20,20 +22,29 @@ const ProfilePage = () => {
 	const coverImgRef = useRef(null);
 	const profileImgRef = useRef(null);
 
-	const isLoading = false;
 	const isMyProfile = true;
 
-	const user = {
-		_id: "1",
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: "/avatars/boy2.png",
-		coverImg: "/cover.png",
-		bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-		link: "https://youtube.com/@asaprogrammer_",
-		following: ["1", "2", "3"],
-		followers: ["1", "2", "3"],
-	};
+	const { username } = useParams();
+
+	const {
+		data: user,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["userProfile"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(`/api/users/profile/${username}`);
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+
+				return data.data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
 
 	const handleImgChange = (e, state) => {
 		const file = e.target.files[0];
@@ -47,16 +58,23 @@ const ProfilePage = () => {
 		}
 	};
 
+	const memberSinceDate = formatMemberSinceDate(user?.createdAt);
+
+	// refetch data when another user is clicked;
+	useEffect(() => {
+		refetch();
+	}, [refetch, username]);
+
 	return (
 		<>
 			<div className="flex-[4_4_0]  border-r border-gray-700 min-h-screen ">
 				{/* HEADER */}
-				{isLoading && <ProfileHeaderSkeleton />}
-				{!isLoading && !user && (
+				{isLoading && isRefetching && <ProfileHeaderSkeleton />}
+				{!isLoading && !isRefetching && !user && (
 					<p className="text-center text-lg mt-4">User not found</p>
 				)}
 				<div className="flex flex-col">
-					{!isLoading && user && (
+					{!isLoading && !isRefetching && user && (
 						<>
 							<div className="flex gap-10 px-4 py-2 items-center">
 								<Link to="/">
@@ -147,16 +165,20 @@ const ProfilePage = () => {
 								</div>
 
 								<div className="flex gap-2 flex-wrap">
-									{user?.link && (
+									{user?.links && (
 										<div className="flex gap-1 items-center ">
 											<>
 												<FaLink className="w-3 h-3 text-slate-500" />
 												<a
-													href="https://youtube.com/@asaprogrammer_"
+													href={
+														user?.links.startsWith("http")
+															? user.links
+															: `http://${user.links}`
+													}
 													target="_blank"
 													rel="noreferrer"
 													className="text-sm text-blue-500 hover:underline">
-													youtube.com/@asaprogrammer_
+													{user?.links}
 												</a>
 											</>
 										</div>
@@ -164,7 +186,7 @@ const ProfilePage = () => {
 									<div className="flex gap-2 items-center">
 										<IoCalendarOutline className="w-4 h-4 text-slate-500" />
 										<span className="text-sm text-slate-500">
-											Joined July 2021
+											{memberSinceDate}
 										</span>
 									</div>
 								</div>
