ddd2dc73ece327bc66982f3ff09a217a24a9d097
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-12 17:37:01 +0300

    prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import connectMongoDB from "./db/connectDB.js";
 import dotenv from "dotenv";
@@ -13,11 +14,9 @@ import notificationRoutes from "./routes/notification.routes.js";
 const app = express();
 
 // middlewares
-app.use(express.json({limit: "5mb"})); // to parse request body as JSON and limit to prevent DOS attacks;
+app.use(express.json({ limit: "5mb" })); // to parse request body as JSON and limit to prevent DOS attacks;
 app.use(express.urlencoded({ extended: true })); // to parse URL-encoded data
 app.use(cookieParser());
-
-
 
 //cloudinary;
 cloudinary.config({
@@ -32,7 +31,17 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+	// serve static files from the frontend build directory
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	// handle any requests that don't match the above routes
+	app.get("/*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
